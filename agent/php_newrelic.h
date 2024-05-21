7948c46e50981acbd0f7a98ff38476f75a7cc077
Michal Nowacki <mnowacki@newrelic.com>
2024-05-21 10:19:58 -0400

    build agent profiler only on demand
    Agent profiler will only be build when requested.

--- agent/php_newrelic.h
+++ agent/php_newrelic.h
@@ -640,6 +640,7 @@ struct {
   nr_hashmap_t* prepared_statements; /* Prepared statement storage */
 } txn_globals;
 
+#ifdef NRPROF
 enum {
   FCALL_BEGIN=0,
   FCALL_END,
@@ -655,6 +656,7 @@ struct {
   uint32_t count[MAX_PROFILED_FUNCS];
   nrtime_t duration[MAX_PROFILED_FUNCS];
 } profing;
+#endif
 
 ZEND_END_MODULE_GLOBALS(newrelic)
 
@@ -688,6 +690,7 @@ extern PHP_GSHUTDOWN_FUNCTION(newrelic);
 
 #define NRTXN(Y) (NRPRG(txn)->Y)
 #define NRTXNGLOBAL(Y) (NRPRG(txn_globals).Y)
+#ifdef NRPROF
 #define NRPROF_START nrtime_t __nrprof_func_start = nr_get_time()
 #define NRPROF_STOP(f) do {  NRPRG(profing).count[f] += 1; NRPRG(profing).duration[f] += nr_time_duration(__nrprof_func_start, nr_get_time()); } while(0)
 #define NRPROF_DUMP(abs_duration, txn_duration) do { \
@@ -700,6 +703,11 @@ extern PHP_GSHUTDOWN_FUNCTION(newrelic);
     nrl_debug(NRL_AGENT, "PHP_TXN_END: " NR_TIME_FMT "us", NRPRG(profing).duration[PHP_TXN_END]); \
     nrl_debug(NRL_AGENT, "POST_DEACTIVATE: " NR_TIME_FMT "us", NRPRG(profing).duration[POST_DEACTIVATE]); \
 } while(0)
+#else
+#define NRPROF_START
+#define NRPROF_STOP(f)
+#define NRPROF_DUMP(abs_duration, txn_duration) {(void)abs_duration; (void)txn_duration;}
+#endif
 
 static inline int nr_php_recording(TSRMLS_D) {
   if (nrlikely((0 != NRPRG(txn)) && (0 != NRPRG(txn)->status.recording))) {
